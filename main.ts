c77528e6c8e4532ce822124f2ea4cd0e4340b66f
Byani23 <82613058+Byani23@users.noreply.github.com>
2021-04-15 22:07:13 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+scene.onHitWall(SpriteKind.Food, function (sprite, location) {
+    sprite.destroy(effects.fire, 100)
+    info.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.destroy(effects.disintegrate, 100)
+})
+let good_taco: Sprite = null
+game.splash("\"catch the taco\"", "\"makecode Tutorial game\"")
+tiles.setTilemap(tilemap`level1`)
+let amy = sprites.create(img`
+    . . . . f f f f . . . . . 
+    . . f f c c c c f f . . . 
+    . f f c c c c c c f f . . 
+    f f c c c c c c c c f f . 
+    f f c c f c c c c c c f . 
+    f f f f f c c c f c c f . 
+    f f f f c c c f c c f f . 
+    f f f f f f f f f f f f . 
+    f f f f f f f f f f f f . 
+    . f f f f f f f f f f . . 
+    . f f f f f f f f f f . . 
+    f e f f f f f f f f e f . 
+    e 4 f 7 7 7 7 7 7 c 4 e . 
+    e e f 6 6 6 6 6 6 f e e . 
+    . . . f f f f f f . . . . 
+    . . . f f . . f f . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(amy, 150, 0)
+amy.setPosition(80, 104)
+info.changeScoreBy(0)
+info.setLife(5)
+game.onUpdateInterval(500, function () {
+    good_taco = sprites.create(img`
+        . . . . . . . e e e e . . . . . 
+        . . . . . e e 4 5 5 5 e e . . . 
+        . . . . e 4 5 6 2 2 7 6 6 e . . 
+        . . . e 5 6 6 7 2 2 6 4 4 4 e . 
+        . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
+        . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
+        . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
+        e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
+        e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
+        e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
+        e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
+        e 5 e c 5 4 5 4 5 5 5 e e . . . 
+        e 5 e e 5 5 5 5 5 4 e . . . . . 
+        4 5 4 e 5 5 5 5 e e . . . . . . 
+        . 4 5 4 5 5 4 e . . . . . . . . 
+        . . 4 4 e e e . . . . . . . . . 
+        `, SpriteKind.Food)
+    good_taco.setPosition(randint(10, 145), 10)
+    good_taco.setVelocity(0, 50)
+})
